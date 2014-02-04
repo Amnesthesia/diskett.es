501bcdd0ee3ccec0dee87b7c9a0cfa1ca4332720
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-02-05 00:18:45 +0100

    Updated databaseInterface and class

--- lib/databaseClass.php
+++ lib/databaseClass.php
@@ -1,8 +1,41 @@
 <?php
 
+require('../interfaces/databaseInterface.php');
+
 class DatabaseHandler implements iDatabase
 {
-	
+
+	private $databaseHandler = NULL;
+
+	public function __construct()
+	{
+
+	}
+
+	public function __destruct()
+	{
+
+	}
+
+	public function insert()
+	{
+
+	}
+
+	public function update()
+	{
+
+	}
+
+	public function read()
+	{
+
+	}
+
+	public function delete()
+	{
+
+	}
 }
 
 

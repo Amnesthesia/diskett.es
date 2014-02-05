51ce2aa472417e2c895b37e3ede94115e32987b3
Halvor Mydske Thoresen <Halvtho@online.no>
2014-02-05 22:29:08 +0100

    added fileHandlerCass.php

---
+++ lib/fileHandlerClass.php
@@ -0,0 +1,34 @@
+<?php
+
+class FileHandler
+{
+	private function unzip($zipFile)
+	{
+		$file = './temp/' . $zipFile;
+		$path = './temp';
+
+		$zip = new ZipArchive;
+		$res = $zip->open($file);
+		
+		if ($res === TRUE) 
+		{
+			$zip->extractTo($path);
+			$zip->close();
+		} 
+		else 
+		{
+			echo "Doh! I couldn't open $file";
+		}
+	}
+	
+	private function deleteTempFiles()
+	{
+		$files = glob('./temp/*.xml'); 
+		foreach($files as $file)
+		{ 
+			if(is_file($file)) unlink($file);
+		}
+	}
+}
+
+?>

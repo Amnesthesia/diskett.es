b39302b1d19d78f4cfe8b8075e28574a0466bebc
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-02-06 18:51:29 +0100

    Changed DatabaseHandler to use Singleton Pattern

--- lib/databaseClass.php
+++ lib/databaseClass.php
@@ -1,30 +1,45 @@
 <?php
 
-error_reporting(-1);
-
 require('../interfaces/databaseInterface.php');
 require('../lib/customException.php');
 
+error_reporting(-1);
 
-class DatabaseHandler extends customException implements iDatabase
+
+/*
+Singleton Pattern 
+*/
+class DatabaseHandler implements iDatabase
 {
-	private $databaseHandler = NULL;
+	public static $dbInstance = NULL;
 	private $dbConfig = array();
 
+	public static function getDbInstance()
+	{
+		if (!isset(DatabaseHandler::$dbInstance))
+		{
+			DatabaseHandler::$dbInstance = new DatabaseHandler();
+		}
 
-	public function __construct()
+		return DatabaseHandler::$dbInstance;
+	}
+
+
+	private function __construct()
 	{
 		$this->dbConfig = parse_ini_file('../config/config.php', true); // Dette må gjøres på en bedre måte.
 
-
-		// Her kan vi bruke en custom error handler
-		$this->databaseHandler = new PDO('mysql:host=' . $this->dbConfig['Database']['Host'] .
-				                         ';dbname=' . $this->dbConfig['Database']['DbName'] .
-				                         ';charset=' . $this->dbConfig['Database']['Charset'],
-				                          $this->dbConfig['Database']['User'], 
-				                          $this->dbConfig['Database']['Password'],
-				                          array(PDO::ATTR_EMULATE_PREPARES => false,
-				                             	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		if (!isset($this->databaseHandler))
+		{
+			// Her kan vi bruke en custom error handler
+			$this->databaseHandler = new PDO('mysql:host=' . $this->dbConfig['Database']['Host'] .
+					                         ';dbname=' . $this->dbConfig['Database']['DbName'] .
+					                         ';charset=' . $this->dbConfig['Database']['Charset'],
+					                          $this->dbConfig['Database']['User'], 
+					                          $this->dbConfig['Database']['Password'],
+					                          array(PDO::ATTR_EMULATE_PREPARES => false,
+					                             	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		}
 	}
 
 	public function __destruct()
@@ -56,8 +71,8 @@ class DatabaseHandler extends customException implements iDatabase
 	}
 }
 
-$db = new DatabaseHandler();
-$db->read('SELECT * FROM `show`');
+$db = DatabaseHandler::getDbInstance();
+#var_dump($db->read('SELECT * FROM `show`'));
 
 
 ?>

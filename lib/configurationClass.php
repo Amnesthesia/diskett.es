1561457a7a5e76218d57ac50b6f431b876806e2c
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-02-10 04:20:49 +0100

    Added a class to handle the configuration file

---
+++ lib/configurationClass.php
@@ -0,0 +1,58 @@
+<?php
+
+define("PATH", substr(__DIR__, 0, -3)); // Absolute path
+
+class Configuration
+{
+	private static $instance = NULL;
+	private static $config = array();
+
+
+	/**
+	 * Create an instance of Configuration
+	 * @return object Configuration object
+	 */
+	public static function getInstance()
+	{
+		if (!isset(Configuration::$instance))
+			Configuration::$instance = new Configuration();
+
+		return Configuration::$instance;
+	}
+
+	/**
+	 * Returns the configuration values
+	 * @param  string $configType
+	 * @return arrar  Configuration data
+	 */
+	public static function getConfig($configType)
+	{
+		if (array_key_exists($configType, Configuration::$config))
+			return Configuration::$config[$configType];
+		else
+			return false;
+	}
+
+	/**
+	 * Set a new configuration value
+	 * @param string $configType
+	 * @param string $value
+	 */
+	public static function setConfig($configType, $value)
+	{
+		// Not yet implemented
+	}
+
+
+	/**
+	 * Reads the configuration details from config.php
+	 * and stores them in the array Configuration::$config
+	 */
+	private function __construct()
+	{		
+		Configuration::$config = parse_ini_file(PATH . '/config/config.php', true);
+	}
+}
+/* Example on how to use this class */
+#$conf = Configuration::getInstance();
+#echo $conf->getConfig('Database')['Host'];

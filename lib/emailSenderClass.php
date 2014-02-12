69d7110563470d1764ea7152b2e2214921a71308
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-02-12 20:30:30 +0100

    Updated with comments

--- lib/emailSenderClass.php
+++ lib/emailSenderClass.php
@@ -7,6 +7,10 @@ class MailSender
 	private static $instance;
 	private $mailConfig = array();
 
+	/**
+	 * Create and return an instance of MailSender class
+	 * @return object MailSender
+	 */
 	public static function getInstance()
 	{
 		if (!isset(MailSender::$instance))
@@ -16,16 +20,18 @@ class MailSender
 	}
 
 	/**
-	 * [sendMail description]
+	 * Generic function for sending mail to users of Epguide
 	 * 
 	 *  NOTE:
 	 *  It is worth noting that the mail() function is not suitable for larger volumes of email in a loop. 
 	 *  This function opens and closes an SMTP socket for each email, which is not very efficient.
-	 *	For the sending of large amounts of email, see the » PEAR::Mail, and » PEAR::Mail_Queue packages. 
-	 * @param  [type] $to      [description]
-	 * @param  [type] $message [description]
-	 * @param  [type] $headers [description]
-	 * @return [type]          [description]
+	 *	For the sending of large amounts of email, see the » PEAR::Mail, and » PEAR::Mail_Queue packages.
+	 *	 
+	 * @param  string $to      Mail address (recipient)
+	 * @param  string $subject Subject of mail
+	 * @param  string $message Message to send
+	 * @param  string $headers Optional headers
+	 * @return integer         True or false, indicating if the mail was sent or not
 	 */
 	public function sendMail($to, $subject, $message, $headers = NULL)
 	{
@@ -36,13 +42,14 @@ class MailSender
 		return mail($to, $subject, $message, $headers, '-f ' . $this->mailConfig['Address']);
 	}
 
-
+	/**
+	 * Get mail specific configuration values
+	 */
 	private function __construct() 
 	{
 		$this->mailConfig = Configuration::getInstance()->getConfig('Mail');
 	}
 }
 
-
-$mailSender = MailSender::getInstance()->sendMail('tommy.ingdal@gmail.com', 'Test mail', 'This is the message');
+MailSender::getInstance()->sendMail('tommy.ingdal@gmail.com', 'Test mail', 'This is the message');
 

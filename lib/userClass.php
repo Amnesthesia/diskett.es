e4b791a993f63ec7b2586b9038901ca382e415bf
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-02-12 06:45:34 +0100

    By popular demand, we can now register users using only the User class.

--- lib/userClass.php
+++ lib/userClass.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once('../interfaces/userInterface.php');
 require_once('databaseClass.php');
+require_once(PATH . '/interfaces/userInterface.php');
 
+/**
+ * This class will represent either an existing user or a new user
+ * The user object can be created without parameters if we want to
+ * get a user's details from the database using the existing function
+ * int DatabaseClass.
+ */
 class User implements iUser
 {
 	// This is used when reading values from database:
@@ -17,6 +23,15 @@ class User implements iUser
 	private $fields = array('email', 'password', 'role_id', 'country_id');
 	private $values = array();
 
+
+	/**
+	 * Used to construct a new user object. The construct will only be 'valid',
+	 * if we create a new user.
+	 * @param string  $_email
+	 * @param string  $_passwd
+	 * @param int     $_roleId
+	 * @param int     $_countryId
+	 */
 	public function __construct($_email = NULL, $_passwd = NULL, $_roleId = 0, $_countryId = NULL)
 	{
 		if (isset($_email) && isset($_passwd) && isset($_countryId)) //New user obj.?
@@ -28,63 +43,115 @@ class User implements iUser
 		}
 	}
 
-
+	/**
+	 * Get the ID for this user
+	 * @return int
+	 */
 	public function getId()
 	{
 		return $this->id;
 	}
 
+	/**
+	 * Get the email address for this user
+	 * @return string
+	 */
 	public function getEmail()
 	{
 		return $this->email;
 	}
 
+	/**
+	 * Get the hashed password for this user
+	 * @return string
+	 */
 	public function getPassword()
 	{
 		return $this->password;
 	}
 
+	/**
+	 * Get the role ID for this user
+	 * @return int
+	 */
 	public function getRoleId()
 	{
 		return $this->role_id;
 	}
 
+	/**
+	 * Get the country ID for this user
+	 * @return int
+	 */
 	public function getCountryId()
 	{
 		return $this->country_id;
 	}
 
 
+	/**
+	 * Set mail address for this user
+	 * @param string $_mail
+	 */
 	public function setEmail($_mail)
 	{
 		$this->email = $_mail;
 	}
 
+	/**
+	 * Set password for this user
+	 * @param string $_password
+	 */
 	public function setPassword($_password)
 	{
 		$this->password = $_password;
 	}
 
+	/**
+	 * Set the role ID for this user
+	 * @param integer $_roleId
+	 */
 	public function setRoleId($_roleId = 0) 
 	{
 		$this->role_id = $_roleId;
 	}
 
+	/**
+	 * Set the country ID for this user
+	 * @param int $_countryId
+	 */
 	public function setCountryId($_countryId) 
 	{
 		$this->country_id = $_countryId;
 	}
 
-
+	/**
+	 * Register/Save this user to the database
+	 * @return int The ID this user was assigned in the database
+	 */
 	public function register()
 	{
 		$db = DatabaseHandler::getInstance();
 		$db->insert('user', $this->fields, $this->values);
 	}
 
-
+	/**
+	 * Hash the user's password so we can store it in the database, safely.
+	 * @param  string $password
+	 * @return string Password hash
+	 */
 	private function createHash($password)
 	{
 		return password_hash($password, PASSWORD_DEFAULT);
 	}
 }
+
+/*
+$db = DatabaseHandler::getInstance();
+$users = $db->readToClass('SELECT * FROM `user`', 'User');
+
+foreach($users as $user)
+{
+	echo $user->getEmail() . ' - ' . $user->getPassword() . '<br />';
+}
+*/

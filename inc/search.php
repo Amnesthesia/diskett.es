9c5c26ba32270df2a467e63c96c06119ab90361d
Tommy Ingdal <tommy.ingdal@gmail.com>
2014-03-16 20:14:26 +0100

    Fixed a problem with tvdb/search function

--- inc/search.php
+++ inc/search.php
@@ -5,6 +5,10 @@ $q = ucwords($_GET['searchquery']);
 $show = new TvDb();
 $show->getShow($q);
 
+$id = $show->getShowId($q);
+
 //Ugly hack, redirect to show page. This HAS to be fixed later on
-if (!$show->getShow($q) != false)
+if ($id != false)
 	Header('Location: index.php?page=details&id=' . $show->getShowId($q));
+else
+	echo "Error: Could not find '" . $q . "' in the database.";

61c49f86bc697ff2f6e4278ecfe597f358a9416d
Amnesthesia <sintpanda@gmail.com>
2014-05-13 11:06:35 +0200

    Added list of recent episodes (within 3 months and in the future) with calendar. Still cant get episode placed on the correct fucking date.

--- front-end/js/controllers/episode_controller.js
+++ front-end/js/controllers/episode_controller.js
@@ -1,4 +1,5 @@
 var EpisodeController = Ember.ObjectController.extend({
+
   destroy: function() {
     if (!confirm('Are you sure?')) return;
     this.get('model').deleteRecord();
@@ -7,9 +8,31 @@ var EpisodeController = Ember.ObjectController.extend({
   },
 
   episodeNum: function(){
-  	var id = this.get('episode_id').split(',');
+  	
+  	var id = this.get('id').split(',');
+
   	return id[2];
-  }.property('episode_id')
+  }.property('episode_id'),
+
+  getTime: function(){
+  	return this.get('date').getTime();
+  }.property('date'),
+
+  showname: function(){
+  	return this.get('show_id');
+  }.property('show'),
+
+  getMonthNumber: function(){
+  	return this.get('date').getMonth();
+  }.property(),
+
+  getYearNumber: function(){
+  	return (1900+this.get('date').getYear());
+  }.property(),
+
+  getDayNumber: function(){
+  	return this.get('date').getDate();
+  }.property()
 
 });
 
